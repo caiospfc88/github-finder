dcd4ab1080223b9117867cb89a4dc65f4cd8812e
caiospfc88 <49125995+caiospfc88@users.noreply.github.com>
2024-05-29 11:29:58 -0300

    criação dos componentes da pagina de repositorios, estilização dos elementos usando css modules e finalizando o projeto.

--- src/routes/Repos.tsx
+++ src/routes/Repos.tsx
@@ -7,6 +7,7 @@ import { useParams } from "react-router-dom";
 import classes from "./Repos.module.css";
 import BackBtn from "../components/BackBtn";
 import Loader from "../components/Loader";
+import Repo from "../components/Repo";
 
 const Repos = () => {
   const { username } = useParams();
@@ -21,16 +22,38 @@ const Repos = () => {
 
       const res = await fetch(`https://api.github.com/users/${username}/repos`);
 
-      const data = res.json();
+      const data = await res.json();
 
       setIsLoading(false);
+
+      let orderedRepos = data.sort(
+        (a: RepoProps, b: RepoProps) => b.stargazers_count - a.stargazers_count
+      );
+
+      orderedRepos = orderedRepos.slice(0, 5);
+
+      setRepos(orderedRepos);
     };
+
+    if (username) {
+      loadRepos(username);
+    }
   }, []);
 
+  if (!repos && isLoading) return <Loader />;
+
   return (
-    <div>
+    <div className={classes.repos}>
       <BackBtn />
-      Repos {username}
+      <h2>Explore os repositórios do usuário: {username}</h2>
+      {repos && repos.length === 0 && <p>Não há repositórios.</p>}
+      {repos && repos.length > 0 && (
+        <div className={classes.repos_container}>
+          {repos.map((repo: RepoProps) => (
+            <Repo key={repo.name} {...repo} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
